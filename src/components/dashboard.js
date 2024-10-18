b2e8f07ef3dc7eed28253c2ad27dba18f98e1e92
RaviRanjan3098 <rr8820776@gmail.com>
2024-10-18 14:23:04 +0530

    date:18/10/24

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -8,7 +8,7 @@ import { BiSolidChess } from "react-icons/bi";
 
 const dashboard = () => {
     return (
-        <div className='bg-[url("/image/image.png")] bg-cover  md:px-8 px-4 h-full bg-center'>
+        <div className='bg-[url("/image/image.png")] bg-cover  md:px-8 px-3 pb-6 h-full bg-center'>
             <div className='flex justify-between items-center pt-2'>
                 {/* logo section */}
                 <div>
@@ -38,42 +38,47 @@ const dashboard = () => {
             {/*card section  */}
             <div className='flex flex-wrap w-full md:py-12 m-auto'>
                 {/* Left section */}
-                <div className='md:w-3/12 w-full flex flex-wrap'>
+                <div className='md:w-4/12 w-full flex flex-wrap sm:mb-0 mb-1'>
                     <div className='w-4/12 items-stretch'>
                         <div className='m-2 border-2 border-pinkwhite h-full rounded-full'>
-                            <div className='bg-black opacity-70 rounded-full pb-4 text-white h-full'>
+                            <div className='bg-black opacity-70 rounded-full pb-4 text-white h-full flex flex-col justify-between'>
                                 <div className='rounded-t-full'>
                                     <img src="/image/Frame_single_1.png" className='rounded-t-full w-full' />
-                                    <p className='font-bold text-pinkwhite text-xs text-center  px-1 break-all'>ALL</p>
+                                    <p className='font-bold text-pinkwhite text-xs text-center px-1 break-all'>ALL</p>
                                     <hr className='border border-gray-800 mx-2 mt-1' />
                                 </div>
-                                <div className='mt-2'>
+
+                                <div className='flex-grow mt-2'>
                                     <PiHandHeartFill className='text-4xl m-auto text-pinkgradient' />
                                     <p className='font-bold text-pinkwhite text-xs text-center px-1 break-all'>FAVORITE</p>
                                     <hr className='border border-gray-800 mx-2 mt-1' />
                                 </div>
-                                <div className='mt-2'>
+
+                                <div className='flex-grow mt-2'>
                                     <FaCircleDollarToSlot className='text-4xl m-auto text-pinkgradient' />
                                     <p className='font-bold text-pinkwhite text-xs text-center px-1 break-all'>SLOTS</p>
                                     <hr className='border border-gray-800 mx-2 mt-1' />
                                 </div>
-                                <div className='mt-2'>
+
+                                <div className='flex-grow mt-2'>
                                     <IoFishSharp className='text-4xl m-auto text-pinkgradient' />
                                     <p className='font-bold text-pinkwhite text-xs text-center px-1 break-all'>FISHING</p>
                                     <hr className='border border-gray-800 mx-2 mt-1' />
                                 </div>
+
                                 <div className='mt-2 bg-black rounded-b-full'>
                                     <BiSolidChess className='text-4xl m-auto text-pinkgradient' />
                                     <p className='font-bold text-pinkwhite text-xs text-center px-1 break-all'>OTHER</p>
-                                    <hr className='border border-gray-800 mx-2 mt-1' />
+                                    <hr className='border border-gray-800 w-1/2 m-auto mt-1' />
                                 </div>
                             </div>
                         </div>
                     </div>
 
+
                     {/* Right section for the image */}
                     <div className='w-8/12 flex items-stretch'>
-                        <div className='m-2 md:mt-0 md:mb-0 h-full'>
+                        <div className='m-2 mr-0 md:mt-0 md:mb-0 h-full'>
                             <img src="/image/wiking.png" className='rounded-md h-full w-full object-cover' />
                         </div>
                     </div>
@@ -81,7 +86,7 @@ const dashboard = () => {
 
 
                 {/* Right section */}
-                <div className='md:w-9/12 w-full flex flex-wrap'>
+                <div className='md:w-8/12 w-full flex flex-wrap items-center'>
                     <div className='w-full sm:w-1/2 md:w-1/3'>
                         <div className='m-2 border-2 border-pinkgradient rounded-md'>
                             <img src="/image/Frame_single.png" className='rounded-md w-full object-cover' />
